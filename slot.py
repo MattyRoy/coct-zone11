709199bef4387e24fac76aa51195cfa679296cd6
Matthew Royle <MattyRoy@users.noreply.github.com>
2021-03-11 17:23:48 +0200

    Update and rename LoadsheddingSlot.py to slot.py

--- slot.py
+++ slot.py
@@ -1,11 +1,9 @@
 from datetime import datetime
-from pytz import timezone
 
 class LoadsheddingSlot:
   def __init__(self, start, end):
     self.start = datetime.strptime(start, "%H:%M").time()
     self.end = datetime.strptime(end, "%H:%M").time()
     
-  def isSlotActive(self):
-    now = datetime.now(timezone("Africa/Johannesburg")).time()
+  def isSlotActive(self, now):
     return self.start <= now and now <= self.end
